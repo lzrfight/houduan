4195d2e0f66577ce9f49e152592cacbd1b725873
lzrfight <1280565957@qq.com>
2020-04-13 23:39:13 +0800

    也忘了改哪了

--- src/main/java/com/bishe/houduan/controller/RegisterController.java
+++ src/main/java/com/bishe/houduan/controller/RegisterController.java
@@ -2,6 +2,7 @@ package com.bishe.houduan.controller;
 
 import com.bishe.houduan.pojo.User;
 import com.bishe.houduan.result.Result;
+import com.bishe.houduan.result.ResultFactory;
 import com.bishe.houduan.service.UserService;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
@@ -27,25 +28,16 @@ public class RegisterController {
         username = HtmlUtils.htmlEscape(username);
         user.setUsername(username);
         boolean exist = userService.isExist(username);
-        if (exist)
-        {
-            return new Result(200);
+        if (exist) {
+            String message = "用户已被使用";
+            return ResultFactory.buildFailResult(message);
         }
         String salt = new SecureRandomNumberGenerator().nextBytes().toString();
         int times = 2;
-        String encodedPassword = new SimpleHash("md5", password , salt , times).toString();
+        String encodedPassword = new SimpleHash("md5", password, salt, times).toString();
         user.setSalt(salt);
         user.setPassword(encodedPassword);
         userService.add(user);
-        if (user == null) {
-            System.out.println("注册失败");
-            return new Result(400);
-        }else {
-            System.out.println("注册成功");
-            System.out.println("注册的账户名为: " + user.getUsername());
-            System.out.println("注册的密码为: " + user.getPassword());
-            return new Result(200);
-
-        }
-        }
+        return ResultFactory.buildSuccessResult(user);
     }
+}
