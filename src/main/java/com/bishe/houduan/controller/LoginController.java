37f3c5cece2c88c18cb6725beeb2d09c4ac23c16
lzrfight <1280565957@qq.com>
2020-04-13 23:38:51 +0800

    忘了加啥功能了

--- src/main/java/com/bishe/houduan/controller/LoginController.java
+++ src/main/java/com/bishe/houduan/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.bishe.houduan.controller;
 
 import com.bishe.houduan.pojo.User;
 import com.bishe.houduan.result.Result;
+import com.bishe.houduan.result.ResultFactory;
 import com.bishe.houduan.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -9,10 +10,7 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -31,13 +29,23 @@ public class LoginController {
 //        subject.getSession().setTimeout(10000);
         UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
 
-        try  {
+        try {
             subject.login(usernamePasswordToken);
             User user = userService.getByName(username);
-            return new Result(200);
-        } catch(AuthenticationException e){
-            System.out.println("账号密码错误");
-            return new Result(400);
+            return ResultFactory.buildSuccessResult(usernamePasswordToken);
+        } catch (AuthenticationException e) {
+            String message = "账号密码错误";
+            return ResultFactory.buildFailResult(message);
         }
+    }
+    @CrossOrigin
+    @ResponseBody
+    @GetMapping(value = "api/logout")
+    public Result logout() {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        String message = "成功退出";
+        System.out.println("成功退出");
+        return ResultFactory.buildSuccessResult(message);
     }
 }
